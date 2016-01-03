939a15b804010f22972d8db7948a44d13a027b5a
george <macbook@MacBook-Pro-macbook-2.local>
2016-01-03 13:20:39 +0200

    deploy

--- config.py
+++ config.py
@@ -3,20 +3,21 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 ## DATABASE CONFIG#######################################
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-if os.environ.get('DATABASE_URL') is None:  ## PRODUCTION
+if os.environ.get('DATABASE_URL') is None:  ## LOCAL
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-    OAUTH_CREDENTIALS = {
-    'facebook': {
-        'id': '1012327082122718',
-        'secret': '29351829c68ccb624e6bc916bee7567d'
-    }
-}    
-else:  #LOCAL
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     OAUTH_CREDENTIALS = {
     'facebook': {
         'id': '416771981865519',
         'secret': 'cda72d0115429b3827bd626aa80c9bd5'
+    }
+}    
+else:  #PRODUCTION
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    OAUTH_CREDENTIALS = {
+    'facebook': {
+            'id': '1012327082122718',
+        'secret': '29351829c68ccb624e6bc916bee7567d'
+
     }
 }    
 
