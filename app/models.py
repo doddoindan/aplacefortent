e378e14b17eabf8802043cb40d529b86e18e756e
george <macbook@MacBook-Pro-macbook-2.local>
2016-01-04 20:38:23 +0200

    deploy

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 from app import db, app
 import re
 
+
+
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
@@ -8,28 +10,29 @@ class User(db.Model):
     nickname = db.Column(db.String(64), unique=True)
     social_id = db.Column(db.String(64), unique=True)
     location = db.Column(db.String(64))
- 
-    def __init__(self, social_id	, email, nickname=None):
+    markers = db.relationship('Marker', backref='owner', lazy = 'dynamic')
+    def __init__(self, social_id, email, nickname=None):
         self.email = email.lower()
         self.social_id = social_id
         self.nickname = nickname
- 
+
     # These four methods are for Flask-Login
     def is_authenticated(self):
         return True
- 
+
     def is_active(self):
         return True
- 
+
     def is_anonymous(self):
         return False
- 
+
     def get_id(self):
         return unicode(self.id)
+
     @staticmethod
     def make_valid_nickname(nickname):
-         return re.sub('[^a-zA-Z0-9_\.]', '', nickname)   
-		        
+        return re.sub('[^a-zA-Z0-9_\.]', '', nickname)
+
     @staticmethod
     def make_unique_nickname(nickname):
         if User.query.filter_by(nickname=nickname).first() is None:
@@ -42,4 +45,20 @@ class User(db.Model):
             version += 1
         return new_nickname
 
-     
+
+class Marker(db.Model):
+    __tablename__ = 'markers'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    long = db.Column(db.Float, nullable=False)
+    latt = db.Column(db.Float, nullable=False)
+    _constr = db.UniqueConstraint("long", "latt")
+
+    @property
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+    def __init__(self, user_id, long, latt):
+        self.user_id = user_id
+        self.long = long
+        self.latt = latt
--- app/views.py
+++ app/views.py
@@ -3,13 +3,34 @@ from flask.ext.login import login_user, logout_user, current_user, login_require
 from app import app, login_manager, db
 from models import User
 from .oauth import OAuthSignIn
+from flask import request
+from flask import Response
+import json
 
+
+##INDEX
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
     return render_template('index.html')
+
+
+## SAVE MARKER
+@app.route('/savemarker', methods=['GET', 'POST'])
+@login_required
+def savemarker():
+    print(request.data)
+
+    ##Return markers list
+    print("_____________")
+    respJson = json.dumps([u.as_dict for u in g.user.markers.all()])
+
+    #xml = '<?xml version="1.0"?><markers><marker name="Aasit" address="Hello Friend !" lat="47.614605" lng="-122.342644" type="house"/><marker name="sad" address="asdasdasd" lat="47.606133" lng="-122.341682" type="restaurant"/><marker name="bjhhjbvjhvbjh" address="jhjkhgjkgjkgkjgjgk" lat="47.615566" lng="-122.341499" type="bar"/><marker name="nh&#xC3;&#xA0; t&#xC3;&#xB4;i" address="h&#xC3;&#xAA; h&#xC3;&#xAA;" lat="47.597469" lng="-122.325050" type="house"/><marker name="tes" address="tes" lat="47.615051" lng="-122.343025" type="restaurant"/><marker name="Moi" address="yes" lat="47.622742" lng="-122.341080" type="house"/></markers>'
+    #return Response(respJson, mimetype='text/xml')
+    return Response(respJson, mimetype='application/json')
+
 
 
 # BEFORE REQUEST PART
@@ -21,6 +42,9 @@ def before_request():
 
 # LOGIN_PART
 
+@app.before_request
+def before_request():
+    g.user = current_user
 
 
 @login_manager.user_loader
